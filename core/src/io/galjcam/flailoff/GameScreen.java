24ec05f4d4cceb4c6fa87c131a2b87a0d9de930a
Cameron <tuh30695@temple.edu>
2020-10-18 01:07:11 -0400

    Added shapes for visual cues

--- core/src/io/galjcam/flailoff/GameScreen.java
+++ core/src/io/galjcam/flailoff/GameScreen.java
@@ -9,8 +9,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -21,20 +24,23 @@ import io.galjcam.flailoff.entities.*;
  * @author Adam Li
  */
 public class GameScreen extends ScreenAdapter {
+
     FlailOff game;
     World world;
     OrthographicCamera camera;
     Box2DDebugRenderer renderer;
-        
+    ShapeRenderer shapeRenderer;
+
     public GameScreen(FlailOff game) {
         this.game = game;
         this.world = new World(new Vector2(0, -10), true);
         this.camera = new OrthographicCamera(852, 480);
         this.renderer = new Box2DDebugRenderer();
+        this.shapeRenderer = new ShapeRenderer();
     }
-    
+
     @Override
-    public void show(){
+    public void show() {
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean keyDown(int keyCode) {
@@ -50,21 +56,29 @@ public class GameScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, .25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
+
         game.background.render(delta);
         game.batch.begin();
         game.font.draw(game.batch, "Game Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
         game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
         game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
         game.batch.end();
+
+        int x = 0, y = 200;
+        int width = 854, height = 80;
         
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(Color.OLIVE);
+        shapeRenderer.rect(x, y, width, height);
+        shapeRenderer.end();
+
         Entity ground = new GroundEntity(world, camera);
         world.step(1 / 60f, 6, 2);
         renderer.render(world, camera.combined);
     }
 
     @Override
-    public void hide(){
+    public void hide() {
         Gdx.input.setInputProcessor(null);
     }
 }
--- core/src/io/galjcam/flailoff/entities/GroundEntity.java
+++ core/src/io/galjcam/flailoff/entities/GroundEntity.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 /**
  *
@@ -35,7 +34,7 @@ public class GroundEntity implements Entity {
         this.groundBox = new PolygonShape();
         // Set the polygon shape as a box which is twice the size of our view port and 20 high
         // (setAsBox takes half-width and half-height as arguments)
-        groundBox.setAsBox(camera.viewportWidth, 10.0f);
+        groundBox.setAsBox(camera.viewportWidth, 40.0f);
         // Create a fixture from our polygon shape and add it to our ground body  
         groundBody.createFixture(groundBox, 0.0f);
         // Clean up after ourselves
